0280d2c39c5c949e3ca4f00ba605f73ecdb99d69
Larina <Losiknor@gmail.com>
2022-03-05 00:30:01 +0700

    Решение задачи 51

--- zadacha_51/Program.cs
+++ zadacha_51/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.
-Console.WriteLine("Hello, World!");
+int[,] array = new int[new Random().Next(3, 10), new Random().Next(3, 10)];
+Console.WriteLine("Задан двумерный массив:");
+Console.WriteLine($"Строк {array.GetLength(0)}; столбцов {array.GetLength(1)}");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i + j;
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
